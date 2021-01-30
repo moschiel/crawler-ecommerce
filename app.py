7c866d8e2bb78f252f1cc82d0e35728fb9665d41
moschiel <moschiel@hotmail.com>
2021-01-30 02:30:39 -0300

    funcionando, porem falta burlar o erro 403

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import utils as u
 import constants as c
 import lxml.html as parser
 import requests
+from time import sleep
 
 
 def setTabUrls(startTab=0, count=0):
@@ -18,35 +19,79 @@ def setTabUrls(startTab=0, count=0):
     return urls
 
 def getPagesPerTab(TabUrls):
-    print("LENDO URL DE PAGINAS DE CADA ABA")
+    print("COLETANDO URL DE PAGINAS DE CADA ABA")
     pages_per_tab = []
-    for tabUrl in TabUrls:
+    for url in TabUrls:
         #read last character
-        chr_tab = tabUrl[-1:]
-        if(chr_tab == "0"):
-            chr_tab = "#"
-
-        print("PAGINAS DA ABA '" + chr_tab + "'")    
+        char_tab = url[-1:] if url[-1:] != "0" else "#" 
         try:
-            page = requests.get(tabUrl, headers=c.HEADERS)
+            sleep(0.5)
+            page = requests.get(url, headers=c.HEADERS)
+            if(page.status_code != 200):
+                print("ERRO NA LEITURA DE PAGINAS DA ABA '" + char_tab + "', STATUS CODE: " + str(page.status_code))
+                continue
+
             tree = parser.fromstring(page.content)
-            page_urls = tree.xpath('//*[@id="summary-pane-'+ chr_tab +'"]/div/div/div/div/ul/li/a/@href')
+            page_urls = tree.xpath('//*[@id="summary-pane-'+ char_tab +'"]/div/div/div/div/ul/li/a/@href')
             page_urls = u.RemoveDuplicates(page_urls) 
             page_urls = u.RemoveIfContain(page_urls, "#")
+            print("coletado " + str(len(page_urls)) + " urls de pagina da aba '" + char_tab + "'")  
+            
             for i in range(len(page_urls)):
                 if(("pagina" not in page_urls[i]) and ("letra" in page_urls[i])):
                     page_urls[i] += "/pagina-1"
                 page_urls[i] = "https://www.americanas.com.br" + page_urls[i]
-                print(page_urls[i])
+                if c.PRINT_PAGE_URL:
+                    print(page_urls[i])
+           
+            pages_per_tab.append(page_urls)
         except:
-            page_urls = "except"
-            print("ERRO NA LEITURA DE PAGINAS DA ABA '" + chr_tab + "'")
-        u.endline() 
+            print("ERRO NA LEITURA DE PAGINAS DA ABA '" + char_tab + "'")
     u.endline() 
     return pages_per_tab
 
+def getSellersPerTab(urlsPerPagePerTab):
+    print ("COLETANDO URL DOS SELLERS DE CADA ABA")
 
-start_tab = 0
-count = 3 #c.MAX_ABAS
+    sellersUrls = []
+    totalSellers = 0
+    for pagesUrls in urlsPerPagePerTab:
+        char_pos = pagesUrls[0].index('letra-') + len('letra-')
+        char_tab = pagesUrls[0][char_pos] if pagesUrls[0][char_pos] != "0" else "#"
+        sellersUrlsCollected = []
+        StatudCodeFail = False
+        for pageUrl in pagesUrls:
+            try:
+                sleep(0.5)
+                page = requests.get(pageUrl, headers=c.HEADERS)
+                if(page.status_code != 200):
+                    #print("ERRO NA LEITURA DOS SELLERS DA ABA '" + char_tab + "', PAGINA " + pageUrl[-1:] + ", STATUS CODE: " + str(page.status_code))
+                    StatudCodeFail = True
+                    continue
+                tree = parser.fromstring(page.content)
+                page_urls = tree.xpath('//*[@id="summary-pane-' + char_tab + '"]/div/ul/li/ul/li/h3/a/@href')
+                sellersUrlsCollected += page_urls
+            except:
+                print("ERRO NA COLETA DE URLS DA PAGINA '" + pageUrl[-1:] + "'")
+        #quando trocavamos de pagina no for acima, muitas urls vem repetido
+        sellersUrlsCollected = u.RemoveDuplicates(sellersUrlsCollected) 
+        #algumas urls falsas geralmente tem a substring '-teste'
+        sellersUrlsCollected = u.RemoveIfContain(sellersUrlsCollected, '-teste') 
+        if c.PRINT_SELLERS_URL:
+            for url in sellersUrlsCollected:
+                print(url)
+        numCollected = len(sellersUrlsCollected)
+        totalSellers += numCollected
+        print("coletado " + str(numCollected) + " urls de sellers da aba '" + char_tab + "' - COMPLETE:" + str(not StatudCodeFail))
+        
+    u.endline()
+    print("TOTAL DE URLS DE SELLERS: " + str(totalSellers))
+    u.endline()
+
+
+
+start_tab = 1
+count = 2
 tabUrls = setTabUrls(start_tab, count)
-getPagesPerTab(tabUrls)
+pagesPerTabUrls = getPagesPerTab(tabUrls)
+getSellersPerTab(pagesPerTabUrls)
--- constants.py
+++ constants.py
@@ -1,4 +1,21 @@
 MAX_ABAS = 27
 PRINT_ABA_URL = True
+PRINT_PAGE_URL = False
+PRINT_SELLERS_URL = False
 
-HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36', "Upgrade-Insecure-Requests": "1","DNT": "1","Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8","Accept-Language": "en-US,en;q=0.5","Accept-Encoding": "gzip, deflate"}
+HEADERS = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36', 
+    #"Upgrade-Insecure-Requests": "1",
+    "dnt": "1",
+    #"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    #accept": "image/avif,image/webp,image/apng,image/svg+xml,image/*,*/*;q=0.8", 
+    "accept": "*/*",
+    "accept-language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7",
+    "accept-encoding": "gzip, deflate, br",
+    "origin": "https://www.americanas.com.br",
+    "referer": "https://www.americanas.com.br/",
+    "sec-ch-ua": '"Chromium";v="88", "Google Chrome";v="88", ";Not A Brand";v="99"',
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "cross-site"
+}
