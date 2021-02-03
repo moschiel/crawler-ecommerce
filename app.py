5fdff688335718ec3893ff40418073bc44b18e25
moschiel <moschiel@hotmail.com>
2021-02-02 21:01:19 -0300

    implementado salvamento e leitura das urls dos sellers

--- app.py
+++ app.py
@@ -26,10 +26,10 @@ def getPagesPerTab(TabUrls):
         #read last character
         char_tab = url[-1:] if url[-1:] != "0" else "#" 
         try:
-            page_urls = f.read_file("pages_tab", "pages_tab_" + char_tab + ".json")
+            page_urls = f.read_file("url_pages_tab", "url_pages_tab_" + char_tab + ".json")
             if(page_urls != False):
                 page_urls = json.loads(page_urls)
-                print("carregado arquivo com " + str(len(page_urls)) + " urls de pagina da aba '" + char_tab + "'") 
+                print("carregado " + str(len(page_urls)) + " urls de pagina da aba '" + char_tab + "'") 
             else:
                 sleep(0.5) #delay pra evitar detecção de requests massivos
                 #intercala entre a url da home e a url que queremos, para assim burlar a detecção de repetição de rotas
@@ -42,8 +42,7 @@ def getPagesPerTab(TabUrls):
                 tree = parser.fromstring(page.content)
                 page_urls = tree.xpath('//*[@id="summary-pane-'+ char_tab +'"]/div/div/div/div/ul/li/a/@href')
                 page_urls = u.RemoveDuplicates(page_urls) 
-                page_urls = u.RemoveIfContain(page_urls, "#")
-                print("salvo arquivo com " + str(len(page_urls)) + " urls de pagina da aba '" + char_tab + "'")  
+                page_urls = u.RemoveIfContain(page_urls, "#")  
             
                 for i in range(len(page_urls)):
                     if(("pagina" not in page_urls[i]) and ("letra" in page_urls[i])):
@@ -51,7 +50,8 @@ def getPagesPerTab(TabUrls):
                     page_urls[i] = "https://www.americanas.com.br" + page_urls[i]
                     if c.PRINT_PAGE_URL:
                         print(page_urls[i])
-                f.save_file("pages_tab", "pages_tab_" + char_tab + ".json", json.dumps(page_urls))
+                f.save_file("url_pages_tab", "url_pages_tab_" + char_tab + ".json", json.dumps(page_urls))
+                print("salvo " + str(len(page_urls)) + " urls de pagina da aba '" + char_tab + "'")
 
             pages_per_tab.append(page_urls)
         except:
@@ -61,41 +61,47 @@ def getPagesPerTab(TabUrls):
 
 def getSellersPerTab(urlsPerPagePerTab):
     print ("COLETANDO URL DOS SELLERS DE CADA ABA")
-
     sellersUrls = []
     totalSellers = 0
     for pagesUrls in urlsPerPagePerTab:
         char_pos = pagesUrls[0].index('letra-') + len('letra-')
         char_tab = pagesUrls[0][char_pos] if pagesUrls[0][char_pos] != "0" else "#"
-        sellersUrlsCollected = []
-        StatudCodeFail = False
-        for pageUrl in pagesUrls:
-            try:
-                sleep(0.5) #delay pra evitar detecção de requests massivos
-                #intercala entre a url da home e a url que queremos, para assim burlar a detecção de repetição de rotas
-                requests.get("https://www.americanas.com.br", headers=c.HEADERS)
-                page = requests.get(pageUrl, headers=c.HEADERS)
+        sellersUrlsCollected = f.read_file("url_sellers", "url_sellers_tab_" + char_tab + ".json")
 
-                if(page.status_code != 200):
-                    #print("ERRO NA LEITURA DOS SELLERS DA ABA '" + char_tab + "', PAGINA " + pageUrl[-1:] + ", STATUS CODE: " + str(page.status_code))
-                    StatudCodeFail = True
-                    continue
-                tree = parser.fromstring(page.content)
-                page_urls = tree.xpath('//*[@id="summary-pane-' + char_tab + '"]/div/ul/li/ul/li/h3/a/@href')
-                sellersUrlsCollected += page_urls
-            except:
-                print("ERRO NA COLETA DE URLS DA PAGINA '" + pageUrl[-1:] + "'")
-        #quando trocavamos de pagina no for acima, muitas urls vem repetido
-        sellersUrlsCollected = u.RemoveDuplicates(sellersUrlsCollected) 
-        #algumas urls falsas geralmente tem a substring '-teste'
-        sellersUrlsCollected = u.RemoveIfContain(sellersUrlsCollected, '-teste') 
-        if c.PRINT_SELLERS_URL:
-            for url in sellersUrlsCollected:
-                print(url)
-        numCollected = len(sellersUrlsCollected)
-        totalSellers += numCollected
-        print("coletado " + str(numCollected) + " urls de sellers da aba '" + char_tab + "' - COMPLETE:" + str(not StatudCodeFail))
-        
+        if(sellersUrlsCollected != False):
+            sellersUrlsCollected = json.loads(sellersUrlsCollected)
+            print("carregado " + str(len(sellersUrlsCollected)) + " urls de sellers da aba '" + char_tab + "'")
+        else:
+            sellersUrlsCollected = []
+            StatudCodeFail = False
+            for pageUrl in pagesUrls:
+                try:
+                    sleep(0.5) #delay pra evitar detecção de requests massivos
+                    #intercala entre a url da home e a url que queremos, para assim burlar a detecção de repetição de rotas
+                    requests.get("https://www.americanas.com.br", headers=c.HEADERS)
+                    page = requests.get(pageUrl, headers=c.HEADERS)
+
+                    if(page.status_code != 200):
+                        StatudCodeFail = True
+                        #continue
+                        print("ERRO NA LEITURA DOS SELLERS DA ABA '" + char_tab + "', PAGINA " + pageUrl[-1:] + ", STATUS CODE: " + str(page.status_code))
+                        return
+                    tree = parser.fromstring(page.content)
+                    page_urls = tree.xpath('//*[@id="summary-pane-' + char_tab + '"]/div/ul/li/ul/li/h3/a/@href')
+                    sellersUrlsCollected += page_urls
+                except:
+                    print("ERRO NA COLETA DE URLS DA PAGINA '" + pageUrl[-1:] + "'")
+            #quando trocavamos de pagina no for acima, muitas urls vem repetido
+            sellersUrlsCollected = u.RemoveDuplicates(sellersUrlsCollected) 
+            #algumas urls falsas geralmente tem a substring '-teste'
+            sellersUrlsCollected = u.RemoveIfContain(sellersUrlsCollected, '-teste') 
+            if c.PRINT_SELLERS_URL:
+                for url in sellersUrlsCollected:
+                    print(url)
+
+            f.save_file("url_sellers", "url_sellers_tab_" + char_tab + ".json", json.dumps(sellersUrlsCollected))
+            print("salvo " + str(len(sellersUrlsCollected)) + " urls de sellers da aba '" + char_tab + "' - COMPLETE:" + str(not StatudCodeFail))
+            
     u.endline()
     print("TOTAL DE URLS DE SELLERS: " + str(totalSellers))
     u.endline()
@@ -107,7 +113,7 @@ def main():
     count = c.MAX_ABAS
     tabUrls = setTabUrls(start_tab, count)
     pagesPerTabUrls = getPagesPerTab(tabUrls)
-    #getSellersPerTab(pagesPerTabUrls)
+    getSellersPerTab(pagesPerTabUrls)
 
 
 if __name__ == '__main__':
